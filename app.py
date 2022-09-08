cb718fb29e28adc82a4a5385d63c779ee0484bac
9111965MA <mubarak.ahhmad@gmail.com>
2022-09-08 13:17:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     hostname = getenv('HOSTNAME')
-    name = getenv('YOUR_NAME')
+    name = getenv('M Ahmad')
     if name is None:
       name = "friend"
     return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n"
